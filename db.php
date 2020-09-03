64ddf2bafb7a351461b956563bd344629800b8e6
Suresh Chand <68882331+sureshchand00a@users.noreply.github.com>
2020-09-03 16:07:37 +0545

    Create db.php

---
+++ db.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * @company : Code Help Pro
+ * @url : https://codehelppro.com
+ * @author : Suresh Chand
+ * @email : info@codehelppro.com
+ * copyright 2020 | All right reserved
+ *
+ * 
+ */
+
+ // ------------------------------------------------------------------------
+ /**
+ * DATABASE SERVER NAME, USUALLY "localhost"
+ */
+ $DB_SERVER = '';
+
+ // ------------------------------------------------------------------------
+ /**
+ * DATABASE USERNAME, YOU MUST CREATE ON YOUR SERVER
+ */
+ $DB_USERNAME = '';
+
+ // ------------------------------------------------------------------------
+ /**
+ * DATABASE PASSWORD, YOU MUST CREATE ON YOUR SERVER
+ */
+ $DB_PASSWORD = '';
+
+ // ------------------------------------------------------------------------
+ /**
+ * DATABASE NAME, YOU MUST CREATE ON YOUR SERVER
+ */
+ $DB = '';
+
+// ------------------------------------------------------------------------
+ /**
+ * DATABASE PREFIX, BY USING THIS, YOU CAN INSTALL THIS PRODUCT MULTIPLE TIMES
+ * IN SINGLE DATABASE. SO, GIVE UNIQUE PREFIX
+ */
+ $PREFIX = '';
